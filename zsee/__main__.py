2ec2e18062857eed4139eebe577633876bb76363
Karen Hambardzumyan <mahnerak@gmail.com>
2019-09-02 03:25:28 +0400

    Colorize Warnings and Errors

--- zsee/__main__.py
+++ zsee/__main__.py
@@ -16,6 +16,8 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.par
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     level=LEVEL)
 
+logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName(logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
 
 def main():
     commands.main('python -m zsee', {
