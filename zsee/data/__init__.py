67ed28579ff339b4ea0f60c85281927fb4b2fa3f
Karen Hambardzumyan <mahnerak@gmail.com>
2019-10-05 00:22:37 +0400

    Parallel TSV Reader for embedding mapping

--- zsee/data/__init__.py
+++ zsee/data/__init__.py
@@ -1,3 +1,4 @@
 from .utils import SpacyRawSentenceSplitter
 from .apf_trigger_reader import APFTriggerReader
 from .bio_trigger_reader import BIOTriggerReader
+from .parallel_tsv_reader import ParallelTSVReader
