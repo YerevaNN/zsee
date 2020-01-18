2fc3671ee63268f9160b00869a683712190c82c2
Karen Hambardzumyan <mahnerak@gmail.com>
2020-01-18 21:46:32 +0400

    Hashing text inputs, for caching

--- zsee/data/utils.py
+++ zsee/data/utils.py
@@ -1,5 +1,8 @@
-from typing import List
+from typing import List, Dict
 
+import torch
+
+from allennlp.data import TokenIndexer, Token, Vocabulary
 from allennlp.data.tokenizers import SentenceSplitter
 from allennlp.data.tokenizers.sentence_splitter import SpacySentenceSplitter
 
@@ -15,3 +18,35 @@ class SpacyRawSentenceSplitter(SpacySentenceSplitter):
 
     def batch_split_sentences(self, texts: List[str]) -> List[List[str]]:
         return [[sentence.string for sentence in doc.sents] for doc in self.spacy.pipe(texts)]
+
+
+@TokenIndexer.register('hash')
+class TextHash(TokenIndexer):
+
+    def count_vocab_items(self, token: Token, counter: Dict[str, Dict[str, int]]):
+        pass
+
+    def tokens_to_indices(
+            self, tokens: List[Token], vocabulary: Vocabulary, index_name: str
+    ) -> Dict[str, List[int]]:
+        immutable_tokens = tuple(tokens)
+        return {
+            "hash": [hash(immutable_tokens)]
+        }
+
+    def get_padding_lengths(self, token: int) -> Dict[str, int]:
+        return {}
+
+    def as_padded_tensor(
+            self,
+            tokens: Dict[str, List[int]],
+            desired_num_tokens: Dict[str, int],
+            padding_lengths: Dict[str, int],
+    ) -> Dict[str, torch.Tensor]:
+        return {
+            key: torch.tensor(val, dtype=torch.long)
+            for key, val in tokens.items()
+        }
+
+    def get_keys(self, index_name: str) -> List[str]:
+        return ["hash"]
