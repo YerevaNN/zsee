2e61a8c37e23bb563f5f0bce06dbba3ecdce56df
Karen Hambardzumyan <mahnerak@gmail.com>
2020-01-18 21:36:55 +0400

    FocalLoss implementation

--- zsee/modules.py
+++ zsee/modules.py
@@ -1,8 +1,9 @@
-import math
+from typing import Type, Union
 
 from overrides import overrides
 from torch import Tensor
 from torch.nn import Module, Parameter, init
+from torch.nn.modules.loss import _WeightedLoss, _Loss
 
 from allennlp.common import FromParams
 from allennlp.modules import Seq2VecEncoder
@@ -43,3 +44,35 @@ class FirstTokenPooler(Seq2VecEncoder):
                 mask: Tensor = None):
         # Collect embeddings of first tokens, presumably [CLS] or similar
         return tokens[:, 0]
+
+
+class FocalLoss(Module):
+
+    def __init__(self,
+                 cls: Union[Type[_WeightedLoss], Type[_Loss]],
+                 *,
+                 gamma: float = 0,
+                 weight: Tensor = None,
+                 reduction: str = 'mean'):
+        super().__init__()
+        self.loss = cls(weight=weight, reduction='none')
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, y_pred, y_true):
+        # Shape: (batch_size, num_classes)
+        loss = self.loss.forward(y_pred, y_true)
+
+        if self.gamma != 0:
+            loss *= (1 - y_pred).pow(self.gamma)  # TODO fix
+
+        if self.reduction == 'mean':
+            # Shape: ()
+            loss = loss.mean()
+        elif self.reduction == 'sum':
+            # Shape: ()
+            loss = loss.sum()
+        else:
+            raise NotImplementedError
+
+        return loss
