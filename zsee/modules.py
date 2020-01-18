1ebab287d867e39648b2ae3de6d8988e5f5a131a
Karen Hambardzumyan <mahnerak@gmail.com>
2020-01-18 21:36:34 +0400

    Custom CLS pooler

--- zsee/modules.py
+++ zsee/modules.py
@@ -5,6 +5,7 @@ from torch import Tensor
 from torch.nn import Module, Parameter, init
 
 from allennlp.common import FromParams
+from allennlp.modules import Seq2VecEncoder
 
 
 class BiasOnly(Module, FromParams):
@@ -20,3 +21,25 @@ class BiasOnly(Module, FromParams):
     @overrides
     def forward(self, inputs: Tensor):
         return inputs + self._bias
+
+
+@Seq2VecEncoder.register("first_token")
+class FirstTokenPooler(Seq2VecEncoder):
+    def __init__(self, embedding_dim: int):
+        super().__init__()
+        self._embedding_dim = embedding_dim
+
+    @overrides
+    def get_input_dim(self) -> int:
+        return self._embedding_dim
+
+    @overrides
+    def get_output_dim(self) -> int:
+        return self._embedding_dim
+
+    @overrides
+    def forward(self,
+                tokens: Tensor,
+                mask: Tensor = None):
+        # Collect embeddings of first tokens, presumably [CLS] or similar
+        return tokens[:, 0]
