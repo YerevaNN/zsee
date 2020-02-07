9a5bfda1a884f62f964b988c71c7e28886759acb
Karen Hambardzumyan <mahnerak@gmail.com>
2020-02-07 13:02:02 +0400

    Callback to log Singular values of certain parameter matrices

--- zsee/callbacks.py
+++ zsee/callbacks.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from typing import List
 
 from torch import Tensor
 
@@ -28,3 +29,27 @@ class OrthonormalizeCallback(Callback):
 
             W.data *= (1 + self.beta)
             W.data -= self.beta * W_hat
+
+
+@Callback.register("log_singular_values")
+class LogSingularValues(Callback):
+
+    def __init__(self,
+                 regex: str) -> None:
+        self.regex = regex
+
+    @handle_event(Events.EPOCH_START)
+    def on_epoch_start(self, trainer):
+        for name, parameter in trainer.model.named_parameters():
+            if not re.search(self.regex, name):
+                continue
+
+            w: Tensor = parameter
+            u, s, v = w.svd(compute_uv=False)
+
+            singular_values = s.detach().cpu().numpy()
+            logger.info(f'Singular Values of {name}:')
+            logger.info(f'{singular_values[0]:.2f}, {singular_values[1]:.2f}, {singular_values[2]:.2f} ... {singular_values[-2]:.2f}, {singular_values[-1]:.2f}')
+            logger.info(f'min={singular_values.min():.2f}, '
+                        f'mean={singular_values.mean():.2f}, '
+                        f'max={singular_values.max():.2f}')
