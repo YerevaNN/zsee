75deb781d8bf31273d722791c2df0ece69d2cafb
Karen Hambardzumyan <mahnerak@gmail.com>
2020-03-02 21:42:41 +0400

    Alignment model with proper Triplet Loss with Semi-Negative Mining, optional symmetrical loss and more verbose reporting

--- zsee/alignment_model.py
+++ zsee/alignment_model.py
@@ -16,6 +16,7 @@ from allennlp.training.metrics import Metric, Average
 @SimilarityFunction.register('mse')
 class MSE(SimilarityFunction):
 
+    @overrides
     def forward(self,
                 tensor_1: torch.Tensor,
                 tensor_2: torch.Tensor) -> torch.Tensor:
@@ -36,6 +37,7 @@ class MinkowskiDistance(SimilarityFunction):
         self.eps = eps
         self.keepdim = keepdim
 
+    @overrides
     def forward(self, tensor_1: torch.Tensor, tensor_2: torch.Tensor) -> torch.Tensor:
         pass
 
@@ -75,7 +77,7 @@ class AlignmentMetric(Metric):
         self.num_correct += num_correct
         self.num_total += batch_size
 
-
+    @overrides
     def get_metric(self, reset: bool) -> Union[float, Tuple[float, ...], Dict[str, float], Dict[str, List[float]]]:
         if self.num_total > 0:
             accuracy = self.num_correct / self.num_total
@@ -87,6 +89,7 @@ class AlignmentMetric(Metric):
 
         return accuracy
 
+    @overrides
     def reset(self):
         self.num_correct = 0
         self.num_total = 0
@@ -103,7 +106,9 @@ class AlignmentModel(Model):
                  map_both: bool = False,
                  models: List[Model] = None,
                  triplet_loss_margin: float = None,
-                 verbose: bool = False) -> None:
+                 version: int = 1,
+                 verbose: bool = False,
+                 symmetric: bool = False) -> None:
         super().__init__(vocab)
 
         # This is hack to share
@@ -123,6 +128,8 @@ class AlignmentModel(Model):
         self._map_both = map_both
         self._triplet_loss_margin = triplet_loss_margin
         self._verbose = verbose
+        self._version = version
+        self._symmetric = symmetric
 
         self._num_samples_seen = 0
         self._average_distance = Average()
@@ -130,31 +137,49 @@ class AlignmentModel(Model):
         self._average_source_pairwise_distance = Average()
         self._average_target_pairwise_distance = Average()
 
+        self._alignments_positive_loss = Average()
+        self._alignments_negative_loss = Average()
+
         initializer(self)
 
     def forward_text(self,
                      text: Dict[str, DataArray],
-                     mapped: bool = True) -> Tensor:
+                     mapped: bool = True):
         # Shape: (batch_size, num_tokens)
         mask = get_text_field_mask(text)
+
         # Shape: (batch_size, num_tokens, embedding_dim)
         mapped_token_embeddings = self._text_field_embedder(text, mapped=mapped)
+
+        if mask.size(1) != mapped_token_embeddings.size(1):
+            mask = (text['bert']['input_ids'] != 0).long()
+
         # # Shape: (batch_size, embedding_dim)
         # source_embeddings: Tensor = self._pooler(source_token_embeddings,
         #                                          mask=source_mask)
         # Shape: (batch_size, embedding_dim)
         mapped_embeddings: Tensor = self._pooler(mapped_token_embeddings,
                                                  mask=mask)
-        return mapped_embeddings
+        return {
+            'mask': mask,
+            'contextual_embeddings': mapped_token_embeddings,
+            'pooled_embeddings': mapped_embeddings
+        }
 
     @overrides
     def forward(self,
                 source_snt: Dict[str, DataArray] = None,
                 target_snt: Dict[str, DataArray] = None,
+                zero_loss: bool = False,
                 **metadata) -> Dict[str, Any]:
         # If `source_snt` is not provided than report nothing.
         if source_snt is None:
             return None
+
+        if self.training and zero_loss:
+            return {
+                'loss': Tensor([0]).float().requires_grad_() * 0
+            }
 
         # Output dict to collect forward results
         output_dict: Dict[str, Any] = dict()
@@ -173,7 +198,8 @@ class AlignmentModel(Model):
         #                                          mask=source_mask)
 
         # Shape: (batch_size, embedding_dim)
-        source_embeddings = self.forward_text(source_snt)
+        output_dict.update(self.forward_text(source_snt))
+        source_embeddings = output_dict['pooled_embeddings']
 
         batch_size = source_embeddings.size(0)
 
@@ -189,16 +215,16 @@ class AlignmentModel(Model):
         #                                          mask=target_mask)
 
         # Shape: (batch_size, embedding_dim)
-        target_embeddings = self.forward_text(target_snt, mapped=self._map_both)
+        target_embeddings = self.forward_text(target_snt, mapped=self._map_both)['pooled_embeddings']
 
         # Shape: (batch_size,)
         distance = self._distance(source_embeddings, target_embeddings)
         # Shape: (batch_size, batch_size)
-        pariwise_distances = self._distance(source_embeddings.unsqueeze(0),
-                                            target_embeddings.unsqueeze(1))
+        pairwise_distances: Tensor = self._distance(source_embeddings.unsqueeze(0),
+                                                    target_embeddings.unsqueeze(1))
         # Shape: (batch_size)
-        random_pair_distances = pariwise_distances[torch.arange(batch_size),
-                                                   torch.randperm(batch_size)]
+        # random_pair_distances = pairwise_distances[torch.arange(batch_size),
+        #                                            torch.randperm(batch_size)]
         # Shape: (batch_size, batch_size)
         source_pairwise_distances = self._distance(source_embeddings.unsqueeze(0),
                                                    source_embeddings.unsqueeze(1))
@@ -206,15 +232,13 @@ class AlignmentModel(Model):
         target_pairwise_distances = self._distance(source_embeddings.unsqueeze(0),
                                                    source_embeddings.unsqueeze(1))
 
-        positive_loss: Tensor = distance.mean()
-        output_dict['positive_loss'] = positive_loss
-        if self._triplet_loss_margin is None:
-            # Shape: ()
-            output_dict['loss'] = positive_loss
-        else:
-            negative_loss: Tensor = random_pair_distances.mean() - self._triplet_loss_margin
-            output_dict['negative_loss'] = negative_loss
-            output_dict['loss'] = (positive_loss - negative_loss).clamp(min=0)
+        if self._version < 2:
+            raise NotImplementedError
+
+        loss = self.alignment_loss_v2(pairwise_distances)
+        if self._symmetric:
+            loss += self.alignment_loss_v2(pairwise_distances.t())
+        output_dict['loss'] = loss
 
         with torch.no_grad():
             self._num_samples_seen += 1
@@ -222,12 +246,33 @@ class AlignmentModel(Model):
             self._alignment_accuracy(source_embeddings, target_embeddings)
 
             self._average_distance(distance.mean().item())
-            self._average_pairwise_distance(pariwise_distances.mean().item())
+            self._average_pairwise_distance(pairwise_distances.mean().item())
             self._average_source_pairwise_distance(source_pairwise_distances.mean().item())
             self._average_target_pairwise_distance(target_pairwise_distances.mean().item())
 
         return output_dict
 
+    def alignment_loss_v2(self, pairwise_distances: Tensor):
+        # Shape: (batch_size, batch_size)
+
+        # Shape: (batch_size, batch_size)
+        positive_loss = pairwise_distances.diagonal()
+
+        inf = pairwise_distances.max().item()
+        # Shape: (batch_size, batch_size)
+        pairwise_distances_masked = pairwise_distances.clone().fill_diagonal_(inf)
+        # Shapes: (batch_size,), _
+        negative_loss, _ = pairwise_distances_masked.min(-1)
+
+        self._alignments_positive_loss(positive_loss.mean().item())
+        self._alignments_negative_loss(negative_loss.mean().item())
+
+        # Shape: (batch_size,)
+        triplet_loss = (positive_loss - negative_loss + self._triplet_loss_margin).clamp(min=0)
+        # Shape: ()
+        return triplet_loss.mean()
+
+    @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         verbose = self._verbose or reset
 
@@ -237,10 +282,18 @@ class AlignmentModel(Model):
         if not self._num_samples_seen:
             return dict()
 
-        return {
+        metrics = {
             'alignment_accuracy': self._alignment_accuracy.get_metric(reset),
-            'average_distance': self._average_distance.get_metric(reset),
-            'average_pairwise_distance': self._average_pairwise_distance.get_metric(reset),
-            'average_source_pairwise_distance': self._average_source_pairwise_distance.get_metric(reset),
-            'average_target_pairwise_distance': self._average_target_pairwise_distance.get_metric(reset)
+            '_distances/average_distance': self._average_distance.get_metric(reset),
+            '_distances/average_pairwise_distance': self._average_pairwise_distance.get_metric(reset),
+            '_distances/average_source_pairwise_distance': self._average_source_pairwise_distance.get_metric(reset),
+            '_distances/average_target_pairwise_distance': self._average_target_pairwise_distance.get_metric(reset),
+
+            '_losses/alignment_positive': self._alignments_positive_loss.get_metric(reset),
+            '_losses/alignment_negative': self._alignments_negative_loss.get_metric(reset)
         }
+
+        if reset:
+            self._num_samples_seen = 0
+
+        return metrics
